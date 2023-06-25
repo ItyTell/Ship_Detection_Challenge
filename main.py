5761ac15e6f734e9d6fc59ec10fddf84f8f3c16f
ItyTell <nickkolomietsdooor@gmail.com>
2023-06-25 17:05:29 +0300

    getting started

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import numpy as np
+import cv2
+from glob import glob
 import tensorflow as tf
+import os
+
+
+batch_size = 8
+learning_rate  = 0.0001
+epochs = 100
+
+path = os.path.join("datasets", "train")
 
